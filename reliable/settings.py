6b68dad832f1b97ca2f450a1a4df8f65f8f56450
Madaraka02 <victormadaraka@gmail.com>
2022-11-02 10:15:37 +0300

    added migration

--- reliable/settings.py
+++ reliable/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d9y*jgxqj0stj$_ovjh$#7285p)zq1+)_&s@saq%7jev=15d2%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
